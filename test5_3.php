9bffe08cddba9639441ecbe5498bbd1c91a34f30
MayumiMitsubiki <Mayumi-mitsubiki@users.noreply.github.com>
2024-07-30 18:40:57 +0900

    test5_3.phpを書いた

---
+++ test5_3.php
@@ -0,0 +1,12 @@
+<?php
+
+$price = 1800;
+$tax_rate = 0.10;
+$price_with_tax = $price * (1 + $tax_rate);
+
+echo "{$price} × 10% = {$price_with_tax} \n";
+
+$family_name = "三引";
+$first_name = "真由美";
+
+echo "{$family_name} {$first_name} \n";
