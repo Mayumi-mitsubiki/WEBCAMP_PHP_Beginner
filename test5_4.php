4a5da0199b7ae02abce1c1fa697e6754dd776a39
MayumiMitsubiki <Mayumi-mitsubiki@users.noreply.github.com>
2024-07-31 15:57:35 +0900

    test5_4.phpを書いた

---
+++ test5_4.php
@@ -0,0 +1,13 @@
+<?php
+
+$num_string = '999';
+$num_int = 999;
+
+var_dump($num_string);
+var_dump($num_int);
+
+// $TEST_FLOAT = 1.23;
+// var_dump($TEST_FLOAT);
+
+define('TEST_FLOAT', 1.23);
+var_dump(TEST_FLOAT);
